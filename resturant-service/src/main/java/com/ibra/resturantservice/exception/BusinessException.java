bcb91c3dbd3505dfdd2780657fad8a0ebea0f8b9
Surface Book <iasafianu@st.ug.edu.gh>
2025-07-05 01:44:15 +0000

    feat: add global exception

---
+++ resturant-service/src/main/java/com/ibra/resturantservice/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.ibra.resturantservice.exception;
+
+
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
